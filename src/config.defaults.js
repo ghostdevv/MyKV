e44f2417423750b59e1c25ae62885002a139bbb6
GHOST <ghostdevbusiness@gmail.com>
2021-05-04 10:45:03 +0100

    feat: config

---
+++ src/config.defaults.js
@@ -0,0 +1,10 @@
+export default {
+    // Database Settings
+    table: 'mykv',
+
+    // Connection settings
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+};
