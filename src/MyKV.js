50f81c0be4703fbcdfda964a85ce06aeac0cee91
GHOST <ghostdevbusiness@gmail.com>
2021-05-04 10:45:10 +0100

    feat: mykv

---
+++ src/MyKV.js
@@ -0,0 +1,88 @@
+import { createConfig, checkRequired } from './helpers/config.js';
+import mysql from 'mysql2/promise';
+import { stringify, parse } from 'json-buffer';
+
+export default class MyKV {
+    #options;
+    #connection;
+
+    constructor(options = {}) {
+        this.#options = createConfig(options);
+
+        const hasRequired = checkRequired(this.#options, [
+            'database',
+            'host',
+            'user',
+            'password',
+        ]);
+
+        if (!hasRequired)
+            throw new Error(
+                'Please check you have all required options: database, host, user, and password',
+            );
+    }
+
+    get open() {
+        const v = this.#connection?.pool?._closed;
+        return v == undefined ? false : !v;
+    }
+
+    async get(key) {
+        const [rows] = await this.#connection.execute(
+            `SELECT * FROM ${this.#options.table} WHERE \`key\` = ? LIMIT 1`,
+            [key],
+        );
+
+        if (rows.length == 0) return undefined;
+        return parse(rows[0].value).data;
+    }
+
+    async set(key, value) {
+        await this.#connection.execute(
+            `INSERT IGNORE INTO ${
+                this.#options.table
+            } (\`key\`, \`value\`) VALUES (?, ?);`,
+            [key, stringify({ data: value })],
+        );
+    }
+
+    async del(key) {
+        await this.#connection.execute(
+            `DELETE FROM ${this.#options.table} WHERE \`key\` = ?`,
+            [key],
+        );
+    }
+
+    async close() {
+        if (!this.open) throw new Error('Connection is already closed');
+        this.#connection.end();
+    }
+
+    async connect() {
+        if (this.open) throw new Error('Connection is already open');
+
+        // Connect
+        this.#connection = mysql.createPool({
+            host: this.#options.host,
+            port: this.#options.post,
+
+            database: this.#options.database,
+
+            user: this.#options.user,
+            password: this.#options.password,
+
+            waitForConnections: this.#options.waitForConnections,
+            connectionLimit: this.#options.connectionLimit,
+            queueLimit: this.#options.queueLimit,
+        });
+
+        // Create the table
+        await this.#connection.execute(
+            `CREATE TABLE IF NOT EXISTS ${
+                this.#options.table
+            } (\`key\` VARCHAR(64) PRIMARY KEY NOT NULL, \`value\` TEXT DEFAULT "{ \\"data\\": null }")`,
+        );
+
+        return;
+    }
+}
