65fc9c2dc2ce33f91302ff7215b04d54519ca76a
GHOST <ghostdevbusiness@gmail.com>
2021-10-13 00:34:10 +0100

    feat: set

--- src/MyKV/MyKV.ts
+++ src/MyKV/MyKV.ts
@@ -1,6 +1,6 @@
 import { MyKVOptions } from '../options/options';
+import { parse, stringify } from 'json-buffer';
 import { BaseMyKV } from './BaseMyKV';
-import { parse } from 'json-buffer';
 
 export class MyKV extends BaseMyKV {
     constructor(options?: MyKVOptions) {
@@ -23,6 +23,18 @@ export class MyKV extends BaseMyKV {
         const value = await this.store.select('value').where({ key }).first();
 
         return value ? parse(value.value) : undefined;
+    }
+
+    async set(key: string, value: any): Promise<void> {
+        if (typeof key != 'string')
+            throw new TypeError(
+                `Key should be a string, recieved ${typeof key}`,
+            );
+
+        await this.store
+            .insert({ key, value: stringify(value) })
+            .onConflict('key')
+            .merge();
     }
 }
 
