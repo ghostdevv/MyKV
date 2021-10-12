00fee6949bc726aa30da1bb9c9813e2da4a76573
GHOST <ghostdevbusiness@gmail.com>
2021-10-12 23:50:54 +0100

    dev: init

---
+++ dev/src/index.js
@@ -0,0 +1,10 @@
+import { MyKV } from 'mykv';
+
+const db = new MyKV({
+    connection: {
+        host: process.env.HOST,
+        user: process.env.USER,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+    },
+});
